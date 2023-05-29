78f45ac3ede3d1da586accf06eda567f3fd3d544
kielsonzinn <kielsonzinn@gmail.com>
2023-05-28 22:10:51 -0300

    create extension acr-logs-qt

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
-# acr-extension-name
+# acr-logs-qt
 
 Arquivo config.json
 
 ```json
 {
-
+  "regexLog": "${CLASS_NAME}::${METHOD_NAME}[ \"].*",
+  "regexFile": ".*\\.h|.*\\.cpp",
+  "logTypes": [
+    "qDebug",
+    "qInfo",
+    "qWarning",
+    "qCritical"
+  ]
 }
+```
+
+Dependencias
+
+- ctags
+
+```shell
+sudo apt-get install universal-ctags
 ```
--- src/review.py
+++ src/review.py
@@ -1,18 +1,134 @@
+import hashlib
+import json
+import os
+import re
+import subprocess
+
+
 def review(config):
-    path_target = config['path_target']
+    log_types = config['logTypes']
+    regex_log = config['regexLog']
+    regex_file = config['regexFile']
     path_source = config['path_source']
 
-    merge = config['merge']
-    project_id = merge['project_id']
-    merge_request_id = merge['merge_request_id']
-    
-    comments = []
-    
-    # TODO IMPLEMENTAR EXTENSION
-    #  O OBJETO DE COMENTARIO DEVE POSSUIR O SEGUINTE FORMATO
-    #  {
-    #      "id": "",
-    #      "comment": ""
-    #  }
+    comments = __review_by_dir(
+        path_code=path_source,
+        path_code_origin=path_source,
+        regex_file=regex_file,
+        regex_log=regex_log,
+        log_types=log_types
+    )
 
     return comments
+
+
+def __review_by_dir(path_code, path_code_origin, regex_file, regex_log, log_types):
+    comments = []
+
+    for content in os.listdir(path_code):
+        path_content = os.path.join(path_code, content)
+
+        if os.path.isfile(path_content):
+            if re.search(regex_file, path_content):
+                comments.extend(__review_by_file(path_code_origin, path_content, regex_log, log_types))
+
+        elif os.path.isdir(path_content):
+            comments.extend(__review_by_dir(path_content, path_code_origin, regex_file, regex_log, log_types))
+
+    return comments
+
+
+def __review_by_file(path_code_origin, path_file, regex_log, log_types):
+    data = subprocess.run(
+        'ctags --output-format=json --format=2 --fields=+line ' + path_file,
+        shell=True,
+        capture_output=True,
+        text=True,
+    ).stdout
+
+    objs = []
+
+    for function in data.split('\n'):
+        if function == '':
+            continue
+
+        function = json.loads(function)
+
+        if function['kind'] != 'function':
+            continue
+
+        if 'scopeKind' not in function or function['scopeKind'] != 'class':
+            continue
+
+        objs.append({
+            'class': function['scope'],
+            'method': function['name'],
+            'startInLine': function['line'],
+            'endInLine': function['end'],
+        })
+
+    objs = sorted(objs, key=lambda x: x['startInLine'])
+
+    with open(path_file, 'r') as f:
+        linhas = f.readlines()
+
+    comments = []
+
+    for obj in objs:
+        start_line = obj['startInLine']
+        end_line = obj['endInLine']
+        check = str(regex_log).replace("${CLASS_NAME}", obj['class']).replace("${METHOD_NAME}", obj['method'])
+
+        for i in range(start_line - 1, end_line):
+            text = linhas[i]
+            text_original = str(text).strip()
+
+            for log_type in log_types:
+                comments.extend(__review_by_line(
+                    text=text,
+                    log_type=log_type,
+                    check=check,
+                    path_file=path_file,
+                    path_code_origin=path_code_origin,
+                    text_original=text_original,
+                    line=i + 1,
+                ))
+
+    return comments
+
+
+def __review_by_line(text, log_type, check, path_file, path_code_origin, text_original, line):
+    comments = []
+
+    if log_type in text:
+        text = text[text.index(log_type) + len(log_type + "() << \""):]
+
+        if not re.search(check, text):
+            path_internal = str(path_file).replace(path_code_origin + "/", "")
+            comment = f"""Linha de código fora do padrão no arquivo: {path_internal}
+Na linha: {line}
+Código fora do padrão:
+```c++
+{text_original}
+```"""
+            comments.append(
+                __create_comment(
+                    comment_id=__generate_md5(text_original),
+                    comment=comment,
+                )
+            )
+
+    return comments
+
+
+def __create_comment(comment_id, comment):
+    return {
+        "id": comment_id,
+        "comment": comment,
+    }
+
+
+def __generate_md5(string):
+    md5_hash = hashlib.md5()
+    md5_hash.update(string.encode('utf-8'))
+    return md5_hash.hexdigest()
